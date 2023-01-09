05049fe6aa08ea1b6b91b4a857f20b1497fc4db8
BC <nancyelorm11@gmail.com>
2023-01-09 21:24:24 +0200

    commit message

--- app/src/main/java/com/example/mytodo/DAO.kt
+++ app/src/main/java/com/example/mytodo/DAO.kt
@@ -1,4 +1,23 @@
-package com.example.mytodo
+package com.example.todo
 
+import androidx.room.*
+import com.example.mytodo.CardInfo
+
+@Dao
 interface DAO {
+    @Insert
+    suspend fun insertTask(entity: Entity)
+
+    @Update
+    suspend fun updateTask(entity: Entity)
+
+    @Delete
+    suspend fun deleteTask(entity: Entity)
+
+    @Query("Delete from to_do")
+    suspend fun deleteAll()
+
+    @Query("Select * from to_do")
+    suspend fun getTasks():List<CardInfo>
+
 }
